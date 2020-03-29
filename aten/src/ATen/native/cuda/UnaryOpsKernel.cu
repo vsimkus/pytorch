6e29386756da1ede4a776f938fdc64ed3baaf7c1
Vaidotas Simkus <simkus.vaidotas@gmail.com>
2020-03-29 19:02:27 +0100

    Add CUDA Aten kernels for clamp().

--- aten/src/ATen/native/cuda/UnaryOpsKernel.cu
+++ aten/src/ATen/native/cuda/UnaryOpsKernel.cu
@@ -94,10 +94,92 @@ void erfinv_kernel_cuda(TensorIterator& iter) {
   });
 }
 
+template<typename scalar_t>
+__host__ __device__ static inline scalar_t zabs_cuda_wrapper(scalar_t v) {
+  return v;
+}
+
+template<typename T>
+__host__ __device__ static inline T zabs_cuda_wrapper(thrust::complex<T> v) {
+  return thrust::abs(v);
+}
+
+void clamp_kernel_cuda(TensorIterator& iter, Scalar min_scalar, Scalar max_scalar) {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND(ScalarType::Half, iter.dtype(), "clamp_cuda", [&]() {
+    using thrust_t = typename ztype_cuda<scalar_t>::thrust_t;
+    auto min = thrust_t(min_scalar.to<scalar_t>());
+    auto max = thrust_t(max_scalar.to<scalar_t>());
+    gpu_kernel(iter,
+      [=]GPU_LAMBDA(thrust_t a) -> thrust_t {
+        return ((zabs_cuda_wrapper(a) < zabs_cuda_wrapper(min)) ?
+                min : ((zabs_cuda_wrapper(a) > zabs_cuda_wrapper(max)) ? max : a));
+    });
+  });
+}
+
+void clamp_max_kernel_cuda(TensorIterator& iter, Scalar max_scalar) {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND(ScalarType::Half, iter.dtype(), "clamp_max_cuda", [&]() {
+    using thrust_t = typename ztype_cuda<scalar_t>::thrust_t;
+    auto max = thrust_t(max_scalar.to<scalar_t>());
+    gpu_kernel(iter,
+      [=]GPU_LAMBDA(thrust_t a) -> thrust_t {
+       return (zabs_cuda_wrapper(a) > zabs_cuda_wrapper(max)) ? max : a;
+    });
+  });
+}
+
+void clamp_min_kernel_cuda(TensorIterator& iter, Scalar min_scalar) {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND(ScalarType::Half, iter.dtype(), "clamp_min_cuda", [&]() {
+    using thrust_t = typename ztype_cuda<scalar_t>::thrust_t;
+    auto min = thrust_t(min_scalar.to<scalar_t>());
+    gpu_kernel(iter,
+      [=]GPU_LAMBDA(thrust_t a) -> thrust_t {
+       return (zabs_cuda_wrapper(a) < zabs_cuda_wrapper(min)) ? min : a;
+    });
+  });
+}
+
+void clamp_with_tensors_kernel_cuda(TensorIterator& iter) {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND(ScalarType::Half, iter.dtype(), "clamp_cuda", [&]() {
+    using thrust_t = typename ztype_cuda<scalar_t>::thrust_t;
+    gpu_kernel(iter,
+      [=]GPU_LAMBDA(thrust_t a, thrust_t min, thrust_t max) -> thrust_t {
+       return ((zabs_cuda_wrapper(a) < zabs_cuda_wrapper(min)) ?
+                min : ((zabs_cuda_wrapper(a) > zabs_cuda_wrapper(max)) ? max : a));
+    });
+  });
+}
+
+void clamp_max_with_tensor_kernel_cuda(TensorIterator& iter) {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND(ScalarType::Half, iter.dtype(), "clamp_max_cuda", [&]() {
+    using thrust_t = typename ztype_cuda<scalar_t>::thrust_t;
+    gpu_kernel(iter,
+      [=]GPU_LAMBDA(thrust_t a, thrust_t max) -> thrust_t {
+       return (zabs_cuda_wrapper(a) > zabs_cuda_wrapper(max)) ? max : a;
+    });
+  });
+}
+
+void clamp_min_with_tensor_kernel_cuda(TensorIterator& iter) {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND(ScalarType::Half, iter.dtype(), "clamp_min_cuda", [&]() {
+    using thrust_t = typename ztype_cuda<scalar_t>::thrust_t;
+    gpu_kernel(iter,
+      [=]GPU_LAMBDA(thrust_t a, thrust_t min) -> thrust_t {
+       return (zabs_cuda_wrapper(a) < zabs_cuda_wrapper(min)) ? min : a;
+    });
+  });
+}
+
 REGISTER_DISPATCH(bitwise_not_stub, &bitwise_not_kernel_cuda);
 REGISTER_DISPATCH(expm1_stub, &expm1_kernel_cuda);
 REGISTER_DISPATCH(rsqrt_stub, &rsqrt_kernel_cuda);
 REGISTER_DISPATCH(sqrt_stub, &sqrt_kernel_cuda);
 REGISTER_DISPATCH(sigmoid_stub, &sigmoid_kernel_cuda);
 REGISTER_DISPATCH(erfinv_stub, &erfinv_kernel_cuda);
+REGISTER_DISPATCH(clamp_stub, &clamp_kernel_cuda);
+REGISTER_DISPATCH(clamp_max_stub, &clamp_max_kernel_cuda);
+REGISTER_DISPATCH(clamp_min_stub, &clamp_min_kernel_cuda);
+REGISTER_DISPATCH(clamp_with_tensors_stub, &clamp_with_tensors_kernel_cuda);
+REGISTER_DISPATCH(clamp_max_with_tensor_stub, &clamp_max_with_tensor_kernel_cuda);
+REGISTER_DISPATCH(clamp_min_with_tensor_stub, &clamp_min_with_tensor_kernel_cuda);
 }}
